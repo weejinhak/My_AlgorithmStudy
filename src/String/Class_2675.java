6875b64f78af543a2992ad7733d114b8be26f73d
weejinhak <wlsgkr91@gmail.com>
2018-07-04 19:42:01 +0900

    Add Class_2675

---
+++ src/String/Class_2675.java
@@ -0,0 +1,40 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Class_2675 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int t = Integer.parseInt(st.nextToken());
+
+        String str = "";
+        String[] bArr = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int rnum = Integer.parseInt(st.nextToken());
+            String inputString = st.nextToken();
+
+            for (int j = 0; j < inputString.length(); j++) {
+                for (int k = 0; k < rnum; k++) {
+                    str += inputString.charAt(j);
+                }
+            }
+            bArr[i] = str;
+            str = "";
+        }
+
+        for (int i = 0; i < t; i++) {
+            System.out.println(bArr[i]);
+        }
+
+    }
+
+}
--- src/String/Ex_StringTokenizer.java
+++ src/String/Ex_StringTokenizer.java
@@ -5,9 +5,9 @@ import java.util.StringTokenizer;
 public class Ex_StringTokenizer {
 
     public static void main(String[] args) {
-        String str = "this%%is%%my%%string";
+        String str = " this is my string ";
 
-        StringTokenizer st = new StringTokenizer(str, "%%");
+        StringTokenizer st = new StringTokenizer(str);
 
         while (st.hasMoreTokens()) {
             System.out.println(st.nextToken());
