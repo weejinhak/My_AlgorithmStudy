191755a2446ed87d171f15313b01aeb1a4a3f262
weejinhak <wlsgkr91@gmail.com>
2018-07-24 20:40:25 +0900

    Add Class 2750

---
+++ src/Sorting/Class_2750.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+
+import java.util.Scanner;
+
+public class Class_2750 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
