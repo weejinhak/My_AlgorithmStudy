42ec047105222560fbe343ede79ebffd02f54361
weejinhak <wlsgkr91@gmail.com>
2018-09-29 01:23:27 +0900

    Code review

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
+        int t = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            boolean[] flag = new boolean[26];
-            String str = br.readLine();
+        String str = "";
+        String[] bArr = new String[t];
 
-            char ch = str.charAt(0);
-            flag[(int) ch - 97] = true;
+        for(int i = 0 ; i < t ; i++){
+            st = new StringTokenizer(br.readLine());
+            int rnum = Integer.parseInt(st.nextToken());
+            String inputString = st.nextToken();
 
-            for (int j = 1; j < str.length(); j++) {
-                if (str.charAt(j) == ch) {
-                    flag[(int) ch - 97] = true;
-                } else {
-                    if (flag[(int) str.charAt(j) - 97]) {
-                        count++;
-                        break;
-                    } else {
-                        flag[(int) str.charAt(j) - 97] = true;
-                        ch = str.charAt(j);
-                    }
+            for(int j = 0 ; j < inputString.length();j++){
+                for(int k = 0 ; k < rnum ; k++){
+                    str += inputString.charAt(j);
                 }
             }
-            System.out.println(N-count);
+            bArr[i] = str;
+            str = "";
+        }
+        for(int i = 0 ; i< t; i++){
+            System.out.println(bArr[i]);
         }
     }
-
 }
