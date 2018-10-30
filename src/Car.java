5290df54f72c3a8ed6008c54b0278dbcbbe88008
weejinhak <wlsgkr91@gmail.com>
2018-10-30 21:40:07 +0900

    Today Algorithm Study

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+    int i = 5;
+    int j = 5;
+
+    Car(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    @Override
+    public String toString() {
+        return "Rar{" +
+                "i=" + i +
+                ", j=" + j +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        String[] arr = {"0","1","2"};
+    public static void main(String[] args){
+        Car car = new Car(10,10);
 
-
-        for(int i = 0 ; i< arr.length;i++){
-            System.out.print(arr[i]);
-        }
-        System.out.println();
-        System.out.println("++++++++");
-        for(String s: arr){
-            System.out.print(s);
-        }
-
+        System.out.println(car.toString());
 
     }
+
 }
